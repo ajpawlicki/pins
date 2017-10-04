66c940459152c5be1ec23f5fc0be14c0d7b5f46d
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-10-04 16:57:48 -0700

    Config webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const DIST = path.join(__dirname, './react-client/dist');
+const SRC = path.join(__dirname, './react-client/src');
+
+module.exports = {
+  entry: `${SRC}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST
+  },
+  module: {
+    loaders: [
+      {
+        include: SRC,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /(\.scss$)/,
+        loaders: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'sass-loader',
+          options: {
+            outputStyle: 'compressed',
+            includePaths: ['./node_modules']
+          }
+        }]
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+      {
+        // IMAGE LOADER
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=public/fonts/[name].[ext]',
+      }
+    ]
+  }
+}
