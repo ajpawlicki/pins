1fd5cecaf8c0f9e9a9e2c39e0d096ae3eba08bbb
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-10-05 01:59:55 -0700

    Make get req for pins and add as state

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,17 +1,36 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
+import PinsList from './components/PinsList.jsx';
+
 // import './style.css';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      pins: [],
+      index: 0
+    }
+  }
+
+  getAllPins() {
+    axios.get('/getAllPins').then(res => this.setState({pins: res.data}));
+  }
+
+  componentDidMount() {
+    this.getAllPins();
   }
 
   render() {
+    console.log('pins:', this.state.pins);
+
     return (
       <div>
-        Pins
+        <b>Pins</b>
+        <PinsList pins={this.state.pins}/>
       </div>
     );
   }
