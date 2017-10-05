c949d5b99f346261866c016d9ecafd0fd7e15105
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-10-05 02:24:16 -0700

    Create route to get only some pins

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const express = require('express');
-
 const app = express();
+
+const bodyParser = require('body-parser');
 
 const pins = require('./data/pins_formatted.json');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/../react-client/dist/'));
 
 app.get('/getAllPins', (req, res) => {
@@ -13,7 +16,9 @@ app.get('/getAllPins', (req, res) => {
 });
 
 app.get('/getMorePins', (req, res) => {
+  const index = +req.query.index;
 
+  res.send(pins.slice(index, index + 10));
 });
 
 app.listen(process.env.PORT || 5000, () => console.log('Listening on port 5000!'));
