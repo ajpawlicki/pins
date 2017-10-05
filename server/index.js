f576143c973a3f3f87e01da9744996f84b671bb2
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-10-05 02:00:53 -0700

    Add routes and handlers for get req

--- server/index.js
+++ server/index.js
@@ -4,6 +4,16 @@ const express = require('express');
 
 const app = express();
 
+const pins = require('./data/pins_formatted.json');
+
 app.use(express.static(__dirname + '/../react-client/dist/'));
+
+app.get('/getAllPins', (req, res) => {
+  res.send(pins);
+});
+
+app.get('/getMorePins', (req, res) => {
+
+});
 
 app.listen(process.env.PORT || 5000, () => console.log('Listening on port 5000!'));
