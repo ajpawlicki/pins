a39268bfdef7e1ae489202d8ec7438102fbcdb90
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-10-05 20:28:41 -0700

    Add anchor tag to pin

--- react-client/src/components/Pin.jsx
+++ react-client/src/components/Pin.jsx
@@ -7,11 +7,16 @@ class Pin extends Component {
   }
 
   render() {
-    const {images, description} = this.props.pin;
+    const {images, description, link} = this.props.pin;
 
     return (
-      <div>
-        <img src={images['236x'].url} alt={description}/>
+      <div className="pin-container">
+        <a href={link}>
+          <div className="pin">
+            <img src={images['236x'].url} alt={description}/>
+            <div className="description">{description}</div>
+          </div>
+        </a>
       </div>
     );
   }
