2eddbec2ff2643706a5ec8845c5b4df9ecc6101b
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-10-06 15:55:27 -0700

    Render pin after image loads and shorten description

--- react-client/src/components/Pin.jsx
+++ react-client/src/components/Pin.jsx
@@ -4,20 +4,41 @@ import ReactDOM from 'react-dom';
 class Pin extends Component {
   constructor(props) {
     super(props);
+
+    this.numOfWords = 9;
+
+    this.state = {
+      imgFinishedLoading: false
+    }
+
+    this.displayPin = this.displayPin.bind(this);
+  }
+
+  displayPin() {
+    this.setState({imgFinishedLoading: true});
+    
+    this.refs.pin.classList.remove('hidden');
+  }
+
+  shortenDescription(text) {
+    const shortened = text.split(' ').slice(0, this.numOfWords).join(' ');
+    return shortened.length < text.length ? shortened.concat('...') : text;
   }
 
   render() {
     const {images, description, link} = this.props.pin;
 
     return (
-      <div className="pin-container">
-        <a href={link}>
+      <li ref="pin" className="pin-container hidden">
+        <a href={link} target="_blank">
           <div className="pin">
-            <img src={images['236x'].url} alt={description}/>
-            <div className="description">{description}</div>
+            <img src={images['236x'].url} alt={description} onLoad={this.displayPin}/>
+            <div className="description">
+              {this.state.imgFinishedLoading ? this.shortenDescription(description) : null}
+            </div>
           </div>
         </a>
-      </div>
+      </li>
     );
   }
 }
