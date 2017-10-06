882f8c5ecea425c77273deeaa8fda38019f7d2f2
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-10-05 20:27:29 -0700

    Handle scroll event and set state according to different events

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import PinsList from './components/PinsList.jsx';
 
-// import './style.css';
+import './style.css';
 import axios from 'axios';
 
 class App extends Component {
@@ -12,8 +12,13 @@ class App extends Component {
 
     this.state = {
       pins: [],
-      index: 0
+      index: 0,
+      loadingData: false,
+      emptyData: false,
+      errorRetrievingData: false
     }
+
+    this.handleScroll = this.handleScroll.bind(this);
   }
 
   getAllPins() {
@@ -21,28 +26,59 @@ class App extends Component {
   }
 
   getMorePins() {
+    this.setState({loadingData: true});
+    
     axios.get(`getMorePins?index=${this.state.index}`)
       .then(res => {
+        if (res.data.length > 0) {
+          this.setState({
+            pins: this.state.pins.concat(res.data),
+            index: this.state.index += 8,
+          });
+          
+        } else {
+          this.setState({emptyData: true});
+        }
+
         this.setState({
-          pins: this.state.pins.concat(res.data),
-          index: this.state.index += 10
+          loadingData: false,
+          errorRetrievingData: false
         });
       })
-      .catch(err => console.error(err));
+      .catch(err => {
+        console.error(err);
+
+        this.setState({errorRetrievingData: true});
+      });
   }
 
   componentDidMount() {
     this.getMorePins();
   }
 
+  handleScroll() {
+    const list = document.querySelector('.list');
+
+    const isScrolledToEnd = list.scrollTop >= (list.scrollHeight - list.offsetHeight);
+
+    if (isScrolledToEnd && !this.state.loadingData && !this.state.emptyData) {
+      this.getMorePins();
+    }
+  }
+
   render() {
-    console.log('pins:', this.state.pins);
-    console.log('index:', this.state.index);
+    // console.log('pins:', this.state.pins);
+    // console.log('index:', this.state.index);
 
     return (
       <div>
-        <b>Pins</b>
-        <PinsList pins={this.state.pins}/>
+        <div className="header"><h1>Pins</h1></div>
+        <div className="container" onScroll={this.handleScroll}>
+          <PinsList pins={this.state.pins}
+            loadingData={this.state.loadingData}
+            emptyData={this.state.emptyData}
+            errorRetrievingData={this.state.errorRetrievingData} />
+        </div>
       </div>
     );
   }
